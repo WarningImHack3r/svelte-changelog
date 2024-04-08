afae3f1cdaa87e940c84a6f790443b475acd1d08
Antoine Lethimonnier <43064022+WarningImHack3r@users.noreply.github.com>
2024-04-08 15:55:46 +0200

    Add svelte-eslint-parser and svelte-devtools

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -85,8 +85,16 @@
 					versionFromTag: tag => tag.substring(tag.indexOf("@") + 1)
 				},
 				{
+					repoName: "svelte-eslint-parser",
+					versionFromTag: tag => tag.replace(/^v/, "")
+				},
+				{
 					repoName: "language-tools",
 					versionFromTag: tag => tag.substring(tag.lastIndexOf("-") + 1)
+				},
+				{
+					repoName: "svelte-devtools",
+					versionFromTag: tag => tag.replace(/^v/, "")
 				}
 			]
 		}
