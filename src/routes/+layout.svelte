WarningImHack3r/svelte-changelog

commit 793045d76322e404edc61e57e3aab0e82e971aa6

Update title markup & dependencies

src/routes/+layout.svelte
@@ -54,12 +54,10 @@
 		<!-- Left part -->
 		<a href="/" class="flex items-center gap-2">
 			<img src="https://svelte.dev/favicon.png" alt="Svelte" class="h-8" />
-			<div class="hidden xs:block">
-				<h2 class="font-semibold xs:text-xl">
-					Svelte
-					<span class="text-primary">Changelog</span>
-				</h2>
-			</div>
+			<h2 class="hidden text-xl xs:inline-block">
+				Svelte
+				<span class="text-primary">Changelog</span>
+			</h2>
 		</a>
 
 		<!-- Right part -->

commit d05ca4da7d634d2dea95ccf7bf5846f6b577d823

Fix title not being bold anymore (regression from 793045d)

src/routes/+layout.svelte
@@ -54,7 +54,7 @@
 		<!-- Left part -->
 		<a href="/" class="flex items-center gap-2">
 			<img src="https://svelte.dev/favicon.png" alt="Svelte" class="h-8" />
-			<h2 class="hidden text-xl xs:inline-block">
+			<h2 class="hidden text-xl font-semibold xs:inline-block">
 				Svelte
 				<span class="text-primary">Changelog</span>
 			</h2>