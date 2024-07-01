f254d6b8928179265ca5b04529a3951e442bb079
Antoine Lethimonnier <43064022+WarningImHack3r@users.noreply.github.com>
2024-07-01 11:38:30 +0200

    Add shortcuts in navbar for category change (#18)

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export type Repo = {
+	/**
+	 * Repository name on GitHub
+	 */
+	repoName: string;
+	/**
+	 * Filter function to apply to the releases of the repo.
+	 * If it returns false, the release is filtered out.
+	 * Could not use Octokit's `data` type because it's not exported.
+	 *
+	 * @param release The release to filter
+	 */
+	dataFilter?: (release: { tag_name: string }) => boolean;
+	/**
+	 * Extracts the version from the tag name.
+	 *
+	 * @param tag The tag name to extract the version from
+	 */
+	versionFromTag: (tag: string) => string;
+};
+
+export type Tab = "svelte" | "kit" | "others";
