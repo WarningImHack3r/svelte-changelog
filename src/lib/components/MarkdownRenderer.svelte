f51cc9cb8bf7c8a80e2605380e23f1766e40405d
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2025-02-08 13:58:04 +0100

    Move custom prose style to MarkdownRenderer.svelte, fix hover modifier order

--- src/lib/components/MarkdownRenderer.svelte
+++ src/lib/components/MarkdownRenderer.svelte
@@ -31,7 +31,7 @@
 <svelte:element
 	this={inline ? "span" : "div"}
 	class={cn(
-		"prose dark:prose-invert prose-a:[overflow-wrap:_anywhere] prose-a:no-underline prose-a:underline-offset-4 hover:prose-a:underline prose-code:[overflow-wrap:_anywhere] prose-li:my-1",
+		"prose dark:prose-invert prose-a:[overflow-wrap:_anywhere] prose-a:text-primary prose-a:no-underline prose-a:underline-offset-4 prose-a:hover:underline prose-code:[overflow-wrap:_anywhere] prose-li:my-1",
 		"prose-pre:text-wrap", // remove with TODO
 		inline && "*:inline",
 		className
