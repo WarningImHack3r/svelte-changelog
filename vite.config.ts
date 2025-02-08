aff912c45b198552502b27139ca41cd94dfc562f
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2025-02-08 01:14:22 +0100

    Initial dependencies migration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import lucidePreprocess from "vite-plugin-lucide-preprocess";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [lucidePreprocess(), sveltekit()],
+	plugins: [lucidePreprocess(), sveltekit(), tailwindcss()],
 	server: {
 		strictPort: true, // default port required for Login with GH workflow
 		fs: {
