cdd04eac1ea2ca4e473db9fd45a22ad776ce68ef
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2025-02-22 12:30:33 +0100

    Initial v2 commit

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
-export const PROD_URL = "https://svelte-changelog.vercel.app";
+export const PROD_URL = "https://next.svelte-changelog.dev";
 export const FAVICON_URL =
 	"https://raw.githubusercontent.com/sveltejs/branding/master/svelte-logo.svg";
 export const FAVICON_PNG_URL = "https://svelte.dev/favicon.png";
