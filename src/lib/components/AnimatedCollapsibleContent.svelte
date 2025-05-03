ed9c618ada80debf504024d933cf9eb93ba1c1e9
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2025-05-04 00:10:52 +0200

    feat: add per-package UI

---
+++ src/lib/components/AnimatedCollapsibleContent.svelte
@@ -0,0 +1,32 @@
+<!--
+@component
+
+Based on https://bits-ui.com/docs/components/collapsible#best-practices
+-->
+<script lang="ts">
+	import { Collapsible, type WithoutChildrenOrChild } from "bits-ui";
+	import { slide, type SlideParams } from "svelte/transition";
+	import type { Snippet } from "svelte";
+
+	let {
+		ref = $bindable(null),
+		duration = 200,
+		axis = undefined,
+		children,
+		...restProps
+	}: WithoutChildrenOrChild<Collapsible.ContentProps> & {
+		duration?: number;
+		axis?: SlideParams["axis"];
+		children?: Snippet;
+	} = $props();
+</script>
+
+<Collapsible.Content forceMount bind:ref {...restProps}>
+	{#snippet child({ props, open })}
+		{#if open}
+			<div {...props} transition:slide={{ duration, axis }} class="flex">
+				{@render children?.()}
+			</div>
+		{/if}
+	{/snippet}
+</Collapsible.Content>
--- src/routes/package/[...package]/+page.svelte
+++ src/routes/package/[...package]/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-	import { navigating } from "$app/state";
-	import { LoaderCircle } from "@lucide/svelte";
+	import { navigating, page } from "$app/state";
+	import { ChevronRight, LoaderCircle, Rss } from "@lucide/svelte";
 	import semver from "semver";
 	import ReleaseCard from "./ReleaseCard.svelte";
+	import { Button } from "$lib/components/ui/button";
+	import { Separator } from "$lib/components/ui/separator";
 	import { Skeleton } from "$lib/components/ui/skeleton";
 	import * as Accordion from "$lib/components/ui/accordion";
+	import * as Collapsible from "$lib/components/ui/collapsible";
+	import AnimatedCollapsibleContent from "$lib/components/AnimatedCollapsibleContent.svelte";
 
 	let { data } = $props();
 	let latestRelease = $derived(
@@ -64,15 +68,48 @@
 					<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 					{@html data.currentPackage.pkg.name.replace(/\//g, "/<wbr />")}
 				</h1>
-				<h2 class="text-xl text-muted-foreground text-shadow-sm/5">
-					<a
-						href="https://github.com/{data.currentPackage.owner}/{data.currentPackage.repoName}"
-						target="_blank"
-						class="underline-offset-2 after:ml-0.5 after:inline-block after:-translate-x-2 after:font-sans after:text-sm after:opacity-0 after:transition after:content-['↗'] hover:underline hover:after:translate-x-0 hover:after:opacity-100"
-					>
-						{data.currentPackage.owner}/<wbr />{data.currentPackage.repoName}
-					</a>
-				</h2>
+				<div class="flex items-center">
+					<h2 class="group text-xl text-muted-foreground text-shadow-sm/5">
+						<a
+							href="https://github.com/{data.currentPackage.owner}/{data.currentPackage.repoName}"
+							target="_blank"
+							class="underline-offset-2 group-hover:underline after:ml-0.5 after:inline-block after:font-sans after:text-sm after:content-['↗']"
+						>
+							{data.currentPackage.owner}/<wbr />{data.currentPackage.repoName}
+						</a>
+					</h2>
+					<Separator orientation="vertical" class="mx-2 h-lh bg-muted-foreground/50" />
+					<Collapsible.Root class="flex items-center">
+						<Collapsible.Trigger>
+							{#snippet child({ props })}
+								<Button
+									{...props}
+									variant="ghost"
+									size="sm"
+									class="peer h-6 px-1 data-[state=open]:text-primary"
+								>
+									<Rss />
+									<span class="sr-only">RSS</span>
+								</Button>
+								<ChevronRight
+									class="size-4 -translate-x-1 scale-75 opacity-0 transition-all peer-hover:translate-x-0 peer-hover:scale-100 peer-hover:opacity-100 peer-data-[state=open]:translate-x-0 peer-data-[state=open]:scale-100 peer-data-[state=open]:rotate-180 peer-data-[state=open]:opacity-100"
+								/>
+							{/snippet}
+						</Collapsible.Trigger>
+						<AnimatedCollapsibleContent axis="x" class="ml-2">
+							{#each [{ name: "XML", file: "rss.xml" }, { name: "ATOM", file: "atom.xml" }, { name: "JSON", file: "rss.json" }] as { name, file } (name)}
+								<Button
+									variant="link"
+									size="sm"
+									class="h-auto px-1 py-0 text-foreground"
+									href="{page.url}/{file}"
+								>
+									{name}
+								</Button>
+							{/each}
+						</AnimatedCollapsibleContent>
+					</Collapsible.Root>
+				</div>
 				{#if data.currentPackage.pkg.description}
 					<h3 class="mt-4 italic">{data.currentPackage.pkg.description}</h3>
 				{/if}
