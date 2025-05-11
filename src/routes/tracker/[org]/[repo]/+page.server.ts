2a76ffddde2695e0efc5caa8767bf841b752b6cb
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2025-05-11 17:59:17 +0200

    feat: remove limit for tracked items

--- src/routes/tracker/[org]/[repo]/+page.server.ts
+++ src/routes/tracker/[org]/[repo]/+page.server.ts
@@ -41,11 +41,9 @@ export async function load({ params }) {
 				}
 				return true;
 			})
-			.toSorted((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime())
-			.slice(0, 10),
+			.toSorted((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()),
 		issues: unfilteredIssues
 			.filter(({ user, pull_request }) => !pull_request && membersNames.includes(user?.login ?? ""))
 			.toSorted((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime())
-			.slice(0, 10)
 	};
 }
