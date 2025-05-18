f94dfaecfd70ca75c95ad3440f309296aaef00cc
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2025-05-18 22:43:11 +0200

    feat(main-page): expand all unseen releases Improve the expanded selection that will replace the old pulsing badges mechanism: - expand only the releases not seen yet since last visit - if never seen, still default to a week prior - always expand the first release if it's newer that a week prior

--- src/routes/package/[...package]/+page.svelte
+++ src/routes/package/[...package]/+page.svelte
@@ -33,8 +33,16 @@
 	);
 	let showPrereleases = $state(true);
 
+	let lastUpdateDate: Date | undefined = undefined;
 	$effect(() => {
-		localStorage.setItem(`last-visited-${data.currentPackage.pkg.name}`, new Date().toISOString());
+		const lastVisit = localStorage.getItem(
+			`last-visited-${data.currentPackage.pkg.name.replace(" ", "-")}`
+		);
+		if (lastVisit) lastUpdateDate = new Date(lastVisit);
+		localStorage.setItem(
+			`last-visited-${data.currentPackage.pkg.name.replace(" ", "-")}`,
+			new Date().toISOString()
+		);
 	});
 </script>
 
@@ -131,10 +139,13 @@
 				type="multiple"
 				value={displayableReleases
 					// Only expand releases that are less than a week old
-					.filter(
-						({ created_at }) =>
-							new Date(created_at).getTime() > Date.now() - 1000 * 60 * 60 * 24 * 7
-					)
+					.filter(({ created_at, published_at }, index) => {
+						const creationTimestamp = new Date(published_at ?? created_at).getTime();
+						const aWeekAgo = Date.now() - 1000 * 60 * 60 * 24 * 7;
+						if (index === 0 && creationTimestamp > aWeekAgo) return true; // always expand the first release if it is recent enough
+						const maxDate = lastUpdateDate?.getTime() ?? aWeekAgo;
+						return creationTimestamp > maxDate;
+					})
 					.map(({ id }) => id.toString())}
 				class="w-full space-y-2"
 			>
