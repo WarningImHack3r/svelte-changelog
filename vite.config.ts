1fda507fc804eff73ea5955924bc76cadd849b89
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2025-06-05 20:44:14 +0200

    chore(deps): add Chrome DevTools Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 import lucidePreprocess from "vite-plugin-lucide-preprocess";
 
 export default defineConfig({
-	plugins: [lucidePreprocess(), sveltekit(), tailwindcss()],
+	plugins: [devtoolsJson(), lucidePreprocess(), sveltekit(), tailwindcss()],
 	server: {
 		strictPort: true // default port required for Login with GH workflow
 	}
