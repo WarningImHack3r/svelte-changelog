80c9cbd2387bb9b498586f729fca5c50b3743728
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2025-06-07 17:44:46 +0200

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from "$app/state";
+</script>
+
+<svelte:head>
+	<title>Error {page.status} | Svelte Changelog</title>
+</svelte:head>
+
+<div class="w-full h-[75vh] flex flex-col justify-center items-center">
+	<h1 class="text-[15rem] leading-none">{page.status}</h1>
+	{#if page.error}
+		<h3 class="font-semibold text-muted-foreground text-2xl">{page.error.message}</h3>
+	{/if}
+</div>
