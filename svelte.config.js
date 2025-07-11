da74a709f60136aaf29fe429f97282fe32cc4038
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2025-07-11 13:40:49 +0200

    chore(deps): update deps, tweak Svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,10 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
-	preprocess: [vitePreprocess({})],
-
+	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter(),
+		// Required for PostHog — https://posthog.com/docs/libraries/svelte#configuring-session-replay-for-server-side-rendered-apps
 		paths: {
 			relative: false
 		}
