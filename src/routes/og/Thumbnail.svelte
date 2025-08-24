6f4a20050098636c6947b64df3f6dfbf542dfcf7
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2025-08-24 19:59:10 +0200

    Initial prototype

---
+++ src/routes/og/Thumbnail.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	type Props = {
+		title: string;
+		description?: string;
+	};
+
+	let { title, description = undefined }: Props = $props();
+</script>
+
+<div class="size-full">
+	<p class="font-display">{title}</p>
+	<p class="text-muted-foreground">{description}</p>
+</div>
