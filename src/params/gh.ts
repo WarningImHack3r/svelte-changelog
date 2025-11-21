73e7772ef31544e72607d88ed7be4188a3595663
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2025-11-21 20:11:56 +0100

    was that it

--- src/params/gh.ts
+++ src/params/gh.ts
@@ -1,5 +1,5 @@
 export function match(param) {
-	const matches = param.replace(/^https?:\/\//, "") === "github.com";
+	const matches = param.replace(/^https?:\/\/?/, "") === "github.com";
 	console.log(`${param} matches? ${matches}.`);
 	return matches;
 }
