b3a0303b2afc4d8c93ed729af1100cc928dafdaa
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2025-11-27 22:46:28 +0100

    fix(details): don't filter out "unknown" packages from the released info
    Spotted thanks to PostHog

--- src/routes/[pid=pid]/[org]/[repo]/[id=number]/+page.server.ts
+++ src/routes/[pid=pid]/[org]/[repo]/[id=number]/+page.server.ts
@@ -2,6 +2,7 @@ import { error, redirect } from "@sveltejs/kit";
 import { resolve } from "$app/paths";
 import { publicRepos, uniqueRepos } from "$lib/repositories";
 import { githubCache } from "$lib/server/github-cache";
+import { discoverer } from "$lib/server/package-discoverer";
 import type { BranchCommit } from "$lib/types";
 
 type Type = "pull" | "issue" | "discussion";
@@ -45,6 +46,7 @@ export async function load({ params: { pid: type, org, repo, id }, fetch }) {
 		item,
 		mergedTagName: new Promise<[string, string] | undefined>((resolve, reject) => {
 			// Credit to Refined GitHub: https://github.com/refined-github/refined-github/blob/main/source/features/closing-remarks.tsx
+
 			// Get the merged PR's sha, otherwise it is not a proper target for this
 			if (!("merged" in item.info)) {
 				resolve(undefined);
@@ -55,23 +57,37 @@ export async function load({ params: { pid: type, org, repo, id }, fetch }) {
 				resolve(undefined);
 				return;
 			}
-			// Fetch the merge commit's info
-			fetch(`https://github.com/${org}/${repo}/branch_commits/${sha}`, {
-				headers: {
-					Accept: "application/json"
-				}
-			})
-				.then(res => res.json() as Promise<BranchCommit>)
-				.then(({ tags }) => {
-					// The info is right here after a little filtering :D
-					const earliestTag = tags.findLast(tag => !tag.includes("nightly") && /\d[.]\d/.test(tag));
-					if (!earliestTag) {
-						resolve(undefined);
-						return;
-					}
-					resolve(matchingRepo?.metadataFromTag(earliestTag));
-				})
-				.catch(reject);
+
+			// Compute or gather all the known packages to avoid rare 404s
+			discoverer
+				.getOrDiscover()
+				.then(items => items.flatMap(({ packages }) => packages).map(({ name }) => name))
+				.then(knownPackages => {
+					const knownPackagesSet = new Set(knownPackages);
+
+					// Fetch the merge commit's info
+					fetch(`https://github.com/${org}/${repo}/branch_commits/${sha}`, {
+						headers: {
+							Accept: "application/json"
+						}
+					})
+						.then(res => res.json() as Promise<BranchCommit>)
+						.then(({ tags }) => {
+							const earliestTag = tags.findLast(tag => {
+								// The info is right here after a little filtering :D
+								const isValid = !tag.includes("nightly") && /\d[.]\d/.test(tag);
+								if (!isValid || !matchingRepo) return false;
+								const [pkgName] = matchingRepo.metadataFromTag(tag);
+								return knownPackagesSet.has(pkgName);
+							});
+							if (!earliestTag) {
+								resolve(undefined);
+								return;
+							}
+							resolve(matchingRepo?.metadataFromTag(earliestTag));
+						})
+						.catch(reject);
+				});
 		})
 	};
 }
