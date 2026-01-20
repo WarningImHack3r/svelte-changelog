84b27f790b1c643ed18d2d786b5d5ba78b1f6f22
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2026-01-20 21:18:28 +0100

    chore(deps): remove vite-plugin-svelte preprocessor, upgrade transitive
    Found out it's not useful by reading sveltejs/cli#876
    
    It's only remove from the direct deps and the Svelte config, though it's
    still used indirectly by SvelteKit

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,7 @@
 import adapter from "@sveltejs/adapter-vercel";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
-	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter(),
 		// Required for PostHog — https://posthog.com/docs/libraries/svelte#configuring-session-replay-for-server-side-rendered-apps
