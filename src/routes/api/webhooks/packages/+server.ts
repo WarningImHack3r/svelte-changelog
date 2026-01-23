ff81f0923091b615038313f6ca7c61fa5f59e6b1
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2026-01-23 20:30:36 +0100

    feat(cache): granularly invalidate cache when receiving webhooks

---
+++ src/routes/api/webhooks/packages/+server.ts
@@ -0,0 +1,28 @@
+import { json } from "@sveltejs/kit";
+import { derror, dlog } from "$lib/debug";
+import { githubCache } from "$lib/server/github-cache";
+import { discoverer } from "$lib/server/package-discoverer";
+import type { ReplicatorEvent } from "./types";
+
+export async function GET() {
+	const discovered = await discoverer.getOrDiscover();
+	const packages = discovered.flatMap(({ packages }) => packages).map(({ name }) => name);
+	return json(packages);
+}
+
+export async function POST({ request }) {
+	const { package: pkg } = (await request.json()) as ReplicatorEvent;
+	const ghUrl = pkg.repository?.url.replace(/\.git$/, "");
+	if (!ghUrl) return new Response(); // always return a success response to avoid the sender to retry
+	const repoFullName = new URL(ghUrl).pathname.slice(1);
+	const [owner, repo] = repoFullName.split("/");
+	if (!owner || !repo) return new Response();
+	dlog(`Received a webhook for ${pkg.name}, invalidating repo ${owner}/${repo}`);
+	const deleted = await githubCache.deleteRepoEntry(owner, repo, "releases");
+	if (deleted) {
+		dlog(`Successfully deleted the entry for ${owner}/${repo}`);
+	} else {
+		derror(`Failed to delete the entry for ${owner}/${repo}`);
+	}
+	return new Response();
+}
