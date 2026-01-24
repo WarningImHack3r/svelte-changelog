f5e009f5526afe94c58cfb7d039e970bb51f1357
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2026-01-24 16:35:29 +0100

    fix(webhooks): change observed event, accurate response codes, cleanup

--- src/routes/api/webhooks/packages/+server.ts
+++ src/routes/api/webhooks/packages/+server.ts
@@ -14,12 +14,14 @@ export async function GET() {
 }
 
 export async function POST({ request }) {
+	// auth
 	const auth = request.headers.get("authorization");
 	if (!auth) return new Response(undefined, { status: 401 });
 	const [, token] = auth.split(" ");
 	if (!token) return new Response(undefined, { status: 401 });
 	if (token !== WEBHOOKS_REPLICATOR_TOKEN) return new Response(undefined, { status: 403 });
 
+	// initial parsing
 	let body: ReplicatorEvent;
 	try {
 		body = (await request.json()) as ReplicatorEvent;
@@ -27,17 +29,17 @@ export async function POST({ request }) {
 		return new Response("Invalid JSON", { status: 400 });
 	}
 
+	// data validation
 	const { event, package: pkg } = body;
-	if (event !== "metadata_updated") return new Response(); // ignoring other events
+	if (event !== "changestream_updated") return new Response(undefined, { status: 415 }); // ignoring other events
 
 	const result = (await discoverer.getOrDiscover()).find(({ packages }) =>
 		packages.some(({ name }) => name === pkg.name)
 	);
-	if (!result) return new Response(); // always return a success response to avoid the sender to retry
+	if (!result) return new Response(undefined, { status: 422 }); // could not found a repo for this package, likely an unknown package
 
+	// final data handling
 	const { repoOwner: owner, repoName: repo } = result;
-	if (!owner || !repo) return new Response();
-
 	dlog(`Received a webhook for ${pkg.name}, invalidating repo ${owner}/${repo}`);
 	const deleted = await githubCache.deleteRepoEntry(owner, repo, "releases");
 	if (deleted) {
