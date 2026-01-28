bcbd6c70f5da00425c5da53b168aaf72f1833fc6
Antoine Lethimonnier <43064022+WarningImHack3r@users.noreply.github.com>
2026-01-28 20:15:33 +0100

    chore(analytics): upload sourcemaps at build-time (#138)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,29 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import posthog from "@posthog/rollup-plugin";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
 import lucidePreprocess from "vite-plugin-lucide-preprocess";
 
-export default defineConfig({
-	plugins: [devtoolsJson(), lucidePreprocess(), sveltekit(), tailwindcss()]
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	const POSTHOG_ENV_ID = env.POSTHOG_ENV_ID;
+	const POSTHOG_SOURCEMAP_API_KEY = env.POSTHOG_SOURCEMAP_API_KEY;
+
+	return {
+		plugins: [
+			devtoolsJson(),
+			lucidePreprocess(),
+			sveltekit(),
+			tailwindcss(),
+			POSTHOG_ENV_ID && POSTHOG_SOURCEMAP_API_KEY
+				? posthog({
+						personalApiKey: POSTHOG_SOURCEMAP_API_KEY,
+						envId: POSTHOG_ENV_ID,
+						host: "https://eu.i.posthog.com"
+					})
+				: undefined
+		]
+	};
 });
