ac1f8a4e0b8f9f3235d02317d8650f79021a4724
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2026-01-28 21:02:58 +0100

    Revert "chore: circumvent route matching quirks"
    This reverts commit b127225e494ffe287ad2b0b8a81dc3e9ee3c36f8.
    
    While the catchall rule prevents unrelated 404 query to get caught by
    the gh route, it also now catches the said gh queries, which completely
    breaks the svcl.dev prefix feature.
    The Svelte team has merged a fix for this route matching bug
    (https://github.com/sveltejs/kit/pull/15216), everything will be sorted
    out shortly naturally.
    For now, I prefer a 403 on /robots.txt than breaking a whole feature I
    myself rely on.

--- src/routes/[...catchall]/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { error } from "@sveltejs/kit";
-
-// required to avoid matching `[...host]` and its subpages
-// see: https://github.com/sveltejs/kit/issues/15204
-export function load() {
-	error(404);
-}
