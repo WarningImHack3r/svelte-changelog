WarningImHack3r/svelte-changelog

commit 72dba4be746ffbaf49dd09e00f828164ba525ba2

feat(sidebar/packages): add the ability to pin packages
Resolves #130

I should do something about all that duplication someday

src/routes/packages/+page.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
 	import { browser } from "$app/environment";
 	import { resolve } from "$app/paths";
-	import { ChevronRight } from "@lucide/svelte";
+	import { ChevronRight, Pin } from "@lucide/svelte";
+	import { PersistedState } from "runed";
 	import type { GitHubRelease } from "$lib/server/github-cache";
 	import { Badge } from "$lib/components/ui/badge";
 	import { Separator } from "$lib/components/ui/separator";
+	import { Toggle } from "$lib/components/ui/toggle";
 
 	let { data } = $props();
+
+	// Pins
+	let pinnedPackages = new PersistedState<string[]>("sidebar-pinned", []);
+	/**
+	 * A set proxy to quickly query the pinned items.
+	 * Storing a customly serialized SvelteSet in the PersistedState doesn't work
+	 * as it isn't reactive (enough).
+	 */
+	const pinnedROProxy = $derived(new Set(pinnedPackages.current));
 
 	/**
 	 * Extract the data from the {@link import('./$types').data.otherReleases|otherReleases}
@@ -57,10 +68,15 @@
 
 <ul class="space-y-8">
 	{#each data.displayablePackages as { category, packages } (category)}
+		{@const sortedPackages = packages.toSorted(({ pkg: pkgA }, { pkg: pkgB }) => {
+			const isAPinned = pinnedROProxy.has(pkgA.name);
+			const isBPinned = pinnedROProxy.has(pkgB.name);
+			return isAPinned === isBPinned ? 0 : isAPinned ? -1 : 1;
+		})}
 		<li>
 			<h3 class="font-display text-3xl text-primary text-shadow-sm">{category.name}</h3>
 			<ul class="mt-2">
-				{#each packages as { repoOwner, repoName, pkg }, index (pkg.name)}
+				{#each sortedPackages as { repoOwner, repoName, pkg }, index (pkg.name)}
 					{@const viewTransitionName = pkg.name.replace(/[@/-]/g, "")}
 					{@const linkedBadgeData = getBadgeDataFromOther(pkg.name)}
 					{#if index > 0}
@@ -71,9 +87,29 @@
 							href={resolve("/package/[...package]", {
 								package: pkg.name
 							})}
-							class="group flex items-center gap-4 rounded-md px-4 py-3 transition-colors hover:bg-neutral-100 dark:hover:bg-neutral-800"
+							onclick={e => {
+								if (!(e.target instanceof HTMLAnchorElement)) e.preventDefault();
+							}}
+							class="group flex items-center gap-4 rounded-md px-4 py-3 transition-colors hover:bg-muted"
 							title={pkg.deprecated ? `Deprecated: ${pkg.deprecated}` : undefined}
 						>
+							{#if sortedPackages.length > 1}
+								<Toggle
+									pressed={pinnedROProxy.has(pkg.name)}
+									class="shrink-0 hover:opacity-100 data-[state=off]:opacity-20 data-[state=off]:group-hover:opacity-50 data-[state=on]:*:fill-amber-500 data-[state=on]:*:stroke-amber-500 hover:data-[state=on]:*:fill-amber-500/30"
+									onPressedChange={pressed => {
+										if (pressed) {
+											if (!pinnedROProxy.has(pkg.name)) pinnedPackages.current.push(pkg.name);
+										} else {
+											pinnedPackages.current = pinnedPackages.current.filter(
+												item => item !== pkg.name
+											);
+										}
+									}}
+								>
+									<Pin />
+								</Toggle>
+							{/if}
 							<div class="flex flex-col">
 								<h4
 									class="inline-flex flex-col items-start gap-2 font-medium motion-safe:[view-transition-name:var(--vt-name)] xs:flex-row xs:items-center"

src/routes/package/SidePanel.svelte
@@ -3,7 +3,8 @@
 	import { browser } from "$app/environment";
 	import { resolve } from "$app/paths";
 	import { page } from "$app/state";
-	import { ChevronRight } from "@lucide/svelte";
+	import { ChevronRight, Pin } from "@lucide/svelte";
+	import { PersistedState } from "runed";
 	import type { GitHubRelease } from "$lib/server/github-cache";
 	import type { CategorizedPackage } from "$lib/server/package-discoverer";
 	import { type PackageSettings, type Prettify, releasesTypes } from "$lib/types";
@@ -13,6 +14,7 @@
 	import { Checkbox } from "$lib/components/ui/checkbox";
 	import { Label } from "$lib/components/ui/label";
 	import { Separator } from "$lib/components/ui/separator";
+	import { Toggle } from "$lib/components/ui/toggle";
 	import * as ToggleGroup from "$lib/components/ui/toggle-group";
 
 	type CleanRelease = { cleanName: string; cleanVersion: string } & GitHubRelease;
@@ -56,6 +58,15 @@
 		class: className
 	}: Props = $props();
 	let id = $props.id();
+
+	// Pins
+	let pinnedPackages = new PersistedState<string[]>("sidebar-pinned", []);
+	/**
+	 * A set proxy to quickly query the pinned items.
+	 * Storing a customly serialized SvelteSet in the PersistedState doesn't work
+	 * as it isn't reactive (enough).
+	 */
+	const pinnedROProxy = $derived(new Set(pinnedPackages.current));
 
 	/**
 	 * Extract the data from the {@link Props.otherReleases|otherReleases}
@@ -140,15 +151,36 @@
 					{/if}
 					<li class="space-y-2">
 						{#if packages.length > 1}
+							{@const sortedPackages = packages.toSorted(({ pkg: pkgA }, { pkg: pkgB }) => {
+								const isAPinned = pinnedROProxy.has(pkgA.name);
+								const isBPinned = pinnedROProxy.has(pkgB.name);
+								return isAPinned === isBPinned ? 0 : isAPinned ? -1 : 1;
+							})}
 							<!-- Categories with sub-items -->
 							<h3 class="text-xl font-bold text-primary" title={category.description}>
 								{category.name}
 							</h3>
 							<ul class="space-y-2">
 								<!-- Sub-items -->
-								{#each packages as { pkg } (pkg.name)}
+								{#each sortedPackages as { pkg } (pkg.name)}
 									{@const linkedBadgeData = getBadgeDataFromOther(pkg.name)}
-									<li>
+									<li class="group inline-flex w-full gap-1">
+										<Toggle
+											size="sm"
+											pressed={pinnedROProxy.has(pkg.name)}
+											class="me-0.5 h-auto min-w-0 shrink-0 px-0 hover:bg-inherit hover:text-inherit hover:opacity-100 data-[state=off]:opacity-20 data-[state=off]:group-hover:opacity-50 data-[state=on]:*:fill-amber-500 data-[state=on]:*:stroke-amber-500 hover:data-[state=on]:*:fill-amber-500/30"
+											onPressedChange={pressed => {
+												if (pressed) {
+													if (!pinnedROProxy.has(pkg.name)) pinnedPackages.current.push(pkg.name);
+												} else {
+													pinnedPackages.current = pinnedPackages.current.filter(
+														item => item !== pkg.name
+													);
+												}
+											}}
+										>
+											<Pin />
+										</Toggle>
 										{#if page.url.pathname.endsWith(`/${pkg.name}`)}
 											<!-- Active sub-item -->
 											<span class="font-semibold">{pkg.name}</span>

commit 67dd4e589513d5a4d2e3450dbe3db690b835428a

fix(packages): allow navigation when clicking on labels & icons
Regression from 72dba4b

Rookie mistake, sorry Paolo

src/routes/packages/+page.svelte
@@ -46,7 +46,7 @@
 								package: pkg.name
 							})}
 							onclick={e => {
-								if (!(e.target instanceof HTMLAnchorElement)) e.preventDefault(); // avoid pinning from navigating
+								if (e.target instanceof HTMLButtonElement) e.preventDefault(); // avoid pinning from navigating
 							}}
 							class="group flex items-center gap-4 rounded-md px-4 py-3 transition-colors hover:bg-muted"
 							title={pkg.deprecated ? `Deprecated: ${pkg.deprecated}` : undefined}