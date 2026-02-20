1fadaa7b78a618158a5ec1b35a24c4c3916f1a2a
Antoine Lethimonnier <43064022+WarningImHack3r@users.noreply.github.com>
2026-02-20 14:23:39 +0100

    fix: please, website, boot

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,17 @@
 import { dev } from "$app/environment";
-import { PUBLIC_POSTHOG_KEY } from "$env/static/public";
+// import { PUBLIC_POSTHOG_KEY } from "$env/static/public";
 import { PostHog } from "posthog-node";
 import { dfatal } from "$lib/debug";
 
-const client = new PostHog(PUBLIC_POSTHOG_KEY, {
+/* const client = new PostHog(PUBLIC_POSTHOG_KEY, {
 	host: "https://eu.i.posthog.com",
 	disabled: dev
-});
+}); */
 
 export async function handleError({ error, status, event, message }) {
 	if (status !== 404) {
 		dfatal(`[SERVER] ${error}`);
-		client.captureException(error, undefined, {
+		/* client.captureException(error, undefined, {
 			...event,
 			status_code: status,
 			error_message: message
@@ -20,11 +20,11 @@ export async function handleError({ error, status, event, message }) {
 			await client.shutdown();
 		} catch {
 			// Mitigate https://github.com/PostHog/posthog-js/issues/2615
-		}
+		} */
 	}
 }
 
 export async function handle({ event, resolve }) {
-	event.locals.posthog = client;
+	// event.locals.posthog = client;
 	return await resolve(event);
 }
