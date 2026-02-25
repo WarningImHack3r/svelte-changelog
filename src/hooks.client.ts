e5ba956608d82a420a18f16c522b6c64e17638e7
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2026-02-25 21:39:34 +0100

    fix(analytics): prevent intentional errors from triggering error hooks
    Temporary mitigation for https://github.com/sveltejs/kit/issues/15425,
    will be reverted once fixed

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -4,6 +4,17 @@ import { dfatal } from "$lib/logging";
 
 export function handleError({ error, status, event, message }) {
 	if (status === 404) return;
+
+	// Mitigate https://github.com/sveltejs/kit/issues/15425
+	if (
+		status === 500 &&
+		typeof error === "object" &&
+		error !== null &&
+		"status" in error &&
+		error.status !== 500
+	)
+		return;
+
 	const stringified = stringify(error);
 	dfatal(`[CLIENT][${status}] ${stringified}`);
 	posthog.captureException(error instanceof Error ? error : new Error(stringified), {
