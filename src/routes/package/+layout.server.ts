434ad326476f2557521d5cff18ef387c22abca1d
Antoine Lethimonnier <43064022+WarningImHack3r@users.noreply.github.com>
2026-04-02 09:23:28 +0200

    fix: add back missing setHeaders
    Blind mobile editing when you got me

--- src/routes/package/+layout.server.ts
+++ src/routes/package/+layout.server.ts
@@ -9,7 +9,7 @@ import { getAllPackagesReleases } from "../all-package-releases";
  * doesn't have to re-run the data loading every time we switch from
  * a package to another.
  */
-export async function load({ locals }) {
+export async function load({ setHeaders, locals }) {
 	// Cache management
 	setHeaders({
 		// one-year cache control, effectively ISR with no expiration
