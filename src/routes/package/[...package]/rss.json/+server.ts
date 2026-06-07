bd70bcb981afbd59401d8b7aab7fef7de27b7ef0
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2026-06-07 23:31:11 +0200

    chore(rss): use new Response instead of text
    Deprecated in SK3

--- src/routes/package/[...package]/rss.json/+server.ts
+++ src/routes/package/[...package]/rss.json/+server.ts
@@ -1,11 +1,11 @@
-import { text } from "@sveltejs/kit";
 import { rssHandler } from "../rss";
 
-export const GET = rssHandler(feed =>
-	text(feed.json1(), {
-		headers: {
-			"Cache-Control": "max-age=0, s-max-age=600",
-			"Content-Type": "application/json"
-		}
-	})
+export const GET = rssHandler(
+	feed =>
+		new Response(feed.json1(), {
+			headers: {
+				"Cache-Control": "max-age=0, s-max-age=600",
+				"Content-Type": "application/json"
+			}
+		})
 );
--- src/routes/package/[...package]/rss.xml/+server.ts
+++ src/routes/package/[...package]/rss.xml/+server.ts
@@ -1,11 +1,11 @@
-import { text } from "@sveltejs/kit";
 import { rssHandler } from "../rss";
 
-export const GET = rssHandler(feed =>
-	text(feed.rss2(), {
-		headers: {
-			"Cache-Control": "max-age=0, s-max-age=600",
-			"Content-Type": "application/xml"
-		}
-	})
+export const GET = rssHandler(
+	feed =>
+		new Response(feed.rss2(), {
+			headers: {
+				"Cache-Control": "max-age=0, s-max-age=600",
+				"Content-Type": "application/xml"
+			}
+		})
 );
