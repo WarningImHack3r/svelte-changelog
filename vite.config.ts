616676e7ed67479bdfaacf5453849965f382911a
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2026-06-10 23:31:35 +0200

    shaker?

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import adapter from "@sveltejs/adapter-node";
 import posthog from "@posthog/rollup-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import { shaker } from "svelte-shaker/vite";
 import { defineConfig, loadEnv } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
 import lucidePreprocess from "vite-plugin-lucide-preprocess";
@@ -13,6 +14,7 @@ export default defineConfig(({ mode }) => {
 		plugins: [
 			devtoolsJson(),
 			lucidePreprocess(),
+			shaker(),
 			sveltekit({
 				adapter: adapter(),
 				// Required for PostHog — https://posthog.com/docs/libraries/svelte#configuring-session-replay-for-server-side-rendered-apps
