2a9f336e11273dcf5eabeb4b887781efa0067cb0
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2026-07-18 16:09:22 +0200

    update to next-8
    - dedicated `error` prop for `+error.svelte`

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,38 +1,40 @@
 <script lang="ts">
-	import { page } from "$app/state";
 	import { siteName } from "$lib/properties";
 	import { Button } from "$lib/components/ui/button";
+	import type { ErrorProps } from "./$types";
+
+	let { error }: ErrorProps = $props();
 </script>
 
 <svelte:head>
-	<title>Error {page.status} | {siteName}</title>
+	<title>Error {error.status} | {siteName}</title>
 </svelte:head>
 
 <div
 	class={[
 		"flex h-[75vh] w-full flex-col justify-center",
-		page.error?.description ? "items-start gap-2" : "items-center"
+		error?.description ? "items-start gap-2" : "items-center"
 	]}
 >
-	{#if page.error?.description}
+	{#if error?.description}
 		<div class="relative w-full">
-			<h1 class="text-5xl leading-none">{page.error.message}</h1>
+			<h1 class="text-5xl leading-none">{error.message}</h1>
 			<h3 class="max-w-prose text-xl font-semibold text-muted-foreground">
-				{page.error.description}
+				{error.description}
 			</h3>
 			<span
 				class="absolute inset-y-0 right-0 -translate-y-4 font-display text-9xl text-muted-foreground opacity-20"
 			>
-				{page.status}
+				{error.status}
 			</span>
 		</div>
 	{:else}
-		<h1 class="text-[15rem] leading-none">{page.status}</h1>
-		{#if page.error}
-			<h3 class="text-2xl font-semibold text-muted-foreground">{page.error.message}</h3>
+		<h1 class="text-[15rem] leading-none">{error.status}</h1>
+		{#if error}
+			<h3 class="text-2xl font-semibold text-muted-foreground">{error.message}</h3>
 		{/if}
 	{/if}
-	{#if page.error?.link}
-		<Button href={page.error.link.href} class="mt-8">{page.error.link.text}</Button>
+	{#if error?.link}
+		<Button href={error.link.href} class="mt-8">{error.link.text}</Button>
 	{/if}
 </div>
