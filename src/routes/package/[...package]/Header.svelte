WarningImHack3r/svelte-changelog

commit 2b37916e4f907ad49696133d7ccecd04360ba18c

fix(home-page): security fix banner being scrambled with the pkg desc.
Prevent the security fix banner from being scrambled with the package's
description, especially on mobile if it's the latest version.

Also fix the banner's width during dev when the release is also the
installed version (+ one ordering change).

src/routes/package/[...package]/Header.svelte
@@ -42,9 +42,8 @@
 			({ iconUrl: string } | { icon: Component }) & { url: string; imgClasses?: string }
 		>
 	>(
-		!packageInfo.isNpmPackage
-			? Object.fromEntries([])
-			: {
+		packageInfo.isNpmPackage
+			? {
 					npmjs: {
 						icon: Npm,
 						url: `https://npmjs.com/package/${packageInfo.name}`
@@ -55,6 +54,7 @@
 						imgClasses: "dark:invert"
 					}
 				}
+			: {}
 	);
 
 	// Description check
@@ -235,7 +235,8 @@
 				element="h3"
 				markdown={packageInfo.description}
 				inline
-				class="mt-4 max-w-full px-0 italic motion-safe:[view-transition-name:var(--vt-name)]"
+				// "relative" + < 10 z-index to go below the security fix notice
+				class="relative -z-15 mt-4 max-w-full px-0 italic motion-safe:[view-transition-name:var(--vt-name)]"
 				additionalPlugins={[{ renderer: { p: Transparent } }]}
 			>
 				{#snippet a({ children, ...rest })}

src/routes/package/[...package]/ReleaseCard.svelte
@@ -353,18 +353,21 @@
 <Accordion.Item
 	id={release.cleanVersion}
 	value={`${release.id}`}
+	style={isLikelySecurityFix
+		? "--dark-destructive-bg: color-mix(in oklab, var(--color-destructive) 25%, var(--color-background))"
+		: undefined}
 	class={[
 		"scroll-mt-20 rounded-md border-b-0 bg-background shadow-lg outline outline-transparent transition-colors duration-300 *:data-accordion-content:rounded-b-md *:data-accordion-content:bg-accent/30 data-[state=open]:outline-muted-foreground/20",
 		{
 			"border border-primary": isMajorRelease && index < 3,
 			"ring ring-primary": page.url.hash && page.url.hash === `#${release.cleanVersion}`,
-			"relative mb-2.5 ring-3 ring-destructive before:absolute before:-top-14 before:left-1/2 before:-z-10 before:w-[98%] before:-translate-x-1/2 before:rounded-t-lg before:border-2 before:border-destructive before:bg-destructive/25 before:px-2 before:pb-1.5 before:text-center before:content-['This_release_likely_contains_security_fixes._Upgrade_as_soon_as_possible!'] sm:before:-top-7.5":
+			"relative mb-2.5 ring-3 ring-destructive before:absolute before:-top-14 before:left-1/2 before:-z-10 before:w-[98%] before:-translate-x-1/2 before:rounded-t-lg before:border-2 before:border-destructive before:bg-(--dark-destructive-bg) before:px-2 before:pb-1.5 before:text-center before:content-['This_release_likely_contains_security_fixes._Upgrade_as_soon_as_possible!'] sm:before:-top-7.5":
 				isLikelySecurityFix,
 			"relative mb-2.5 ring-4 ring-primary after:absolute after:-top-6 after:-left-1 after:-z-10 after:rounded-t-lg after:bg-primary after:px-2 after:pb-1.5 after:font-display after:text-white after:content-['Current_version'] after:text-shadow-xs/50 after:text-shadow-black":
 				dev && versionedPackages[release.cleanName] === release.cleanVersion,
 			"mt-8": dev && versionedPackages[release.cleanName] === release.cleanVersion && index > 0,
 			"mt-17 sm:mt-10": isLikelySecurityFix && index > 0,
-			"before:ml-16 before:w-[85%]":
+			"before:ml-16 before:w-[70%] sm:before:w-[75%] md:before:w-[80%] lg:before:w-[75%] xl:before:w-[85%]":
 				dev && isLikelySecurityFix && versionedPackages[release.cleanName] === release.cleanVersion
 		}
 	]}

commit de115133ed05f06249b3e3a4e9536f6d676cbde7

fix(home-page): header links not being clickable anymore
Regression from 2b37916

src/routes/package/[...package]/Header.svelte
@@ -240,7 +240,7 @@
 				markdown={packageInfo.description}
 				inline
 				// "relative" + < 10 z-index to go below the security fix notice
-				class="relative -z-15 mt-2 max-w-full px-0 motion-safe:[view-transition-name:var(--vt-name)]"
+				class="relative -z-15 mt-2 max-w-full px-0 motion-safe:[view-transition-name:var(--vt-name)] sm:z-[unset] sm:[display:initial]"
 				additionalPlugins={[{ renderer: { p: Transparent } }]}
 			>
 				{#snippet a({ children, ...rest })}