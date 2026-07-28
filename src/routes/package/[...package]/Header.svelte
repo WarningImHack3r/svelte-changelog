WarningImHack3r/svelte-changelog

commit de115133ed05f06249b3e3a4e9536f6d676cbde7

fix(home-page): header links not being clickable anymore
Regression from 2b37916

src/routes/package/[...package]/Header.svelte
@@ -240,7 +240,7 @@
 				markdown={packageInfo.description}
 				inline
 				// "relative" + < 10 z-index to go below the security fix notice
-				class="relative -z-15 mt-2 max-w-full px-0 motion-safe:[view-transition-name:var(--vt-name)]"
+				class="relative -z-15 mt-2 max-w-full px-0 motion-safe:[view-transition-name:var(--vt-name)] sm:z-[unset] sm:[display:initial]"
 				additionalPlugins={[{ renderer: { p: Transparent } }]}
 			>
 				{#snippet a({ children, ...rest })}

commit 4d549deeec0b39e04ca176d20b34790671ef79e7

fix(home-page): header description missing its top margin
Regression from de11513

src/routes/package/[...package]/Header.svelte
@@ -240,7 +240,7 @@
 				markdown={packageInfo.description}
 				inline
 				// "relative" + < 10 z-index to go below the security fix notice
-				class="relative -z-15 mt-2 max-w-full px-0 motion-safe:[view-transition-name:var(--vt-name)] sm:z-[unset] sm:[display:initial]"
+				class="relative -z-15 mt-2 max-w-full px-0 motion-safe:[view-transition-name:var(--vt-name)] sm:z-[unset] sm:[display:revert]"
 				additionalPlugins={[{ renderer: { p: Transparent } }]}
 			>
 				{#snippet a({ children, ...rest })}